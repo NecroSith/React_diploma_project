1c73f20bcf7183802cdb8155efb7b23dd07cf0bd
Yan_Pustynnyy <necrosith@yandex.ru>
2019-07-18 21:03:58 +0300

    Inputs in contact form are now colored when entering data

--- diploma-project/src/components/contact-form/contact-form.js
+++ diploma-project/src/components/contact-form/contact-form.js
@@ -14,15 +14,12 @@ class ContactForm extends React.Component {
 
     onFormSubmit = (e) => {
         e.preventDefault();
-        let errorState = false;
-        if (errorState === false) {
-            const {coffeeService} = this.props;
-            coffeeService.postData(this.state)
-                .then(res => res.json())
-                .then(this.props.formSent)
-                .catch(err => new Error(err));
-            this.clearForm(e);
-        }
+        const {coffeeService} = this.props;
+        coffeeService.postData(this.state)
+            .then(res => res.json())
+            .then(this.props.formSent)
+            .catch(err => new Error(err));
+        this.clearForm(e);
     }
 
     clearForm = (e) => {
@@ -33,22 +30,34 @@ class ContactForm extends React.Component {
     }
 
     validateData(data, type) {
-        console.log(this.state);
-        const input = data.target.value;
+        const input = data.target.value.trim();
+        let inputBlock = data.target;
+        if (type === 'phone') {
+            if (input.length !== 0) {
+                inputBlock.style.borderColor = 'green';
+                this.setState({
+                    phone: input
+                })
+            }
+        }
         if (type === 'name') {
             let nameField = document.querySelector('.error-name');
             if (input.length < 2 && input !== '') {
                 nameField.textContent = 'Name is too short!';
+                inputBlock.style.borderColor = 'red';
                 return false;
             }
             else if (input.length > 20) {
                 nameField.textContent = 'Name is too long';
+                inputBlock.style.borderColor = 'red';
                 return false;
             }
             else if (input === '') {
                 nameField.textContent = null;
+                inputBlock.style.borderColor = 'grey';
                 return false;
             }
+            inputBlock.style.borderColor = 'green';
             nameField.textContent = null;
             this.setState({
                 name: input
@@ -60,6 +69,7 @@ class ContactForm extends React.Component {
             const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             const result = re.test(String(input).toLowerCase());
             if (result || input.length === 0) {
+                inputBlock.style.borderColor = 'green';
                 emailField.textContent = null;
                 this.setState({
                     email: input
@@ -67,15 +77,18 @@ class ContactForm extends React.Component {
                 return true;
             }
             emailField.textContent = 'Email is not valid!';
+            inputBlock.style.borderColor = 'red';
             return false;
         }
         else if (type === 'message') {
             let msgField = document.querySelector('.error-message');
             if (input !== '' && input.length < 2) {
+                inputBlock.style.borderColor = 'red';
                 msgField.textContent = 'Message is too short!';
                 return false;
             }
             msgField.textContent = null;
+            inputBlock.style.borderColor = 'green';
             this.setState({
                 message: input
             });
@@ -83,30 +96,29 @@ class ContactForm extends React.Component {
         }
     }
 
-
     render() {
 
         return (
             <form className="form" onSubmit={this.onFormSubmit}>
                 <div>
                     <label htmlFor="name" className="required">Name</label>
-                    <input type="name" className="error" name="name" required onChange={(e) => this.validateData(e, 'name')}/>
                     <span className="error-field error-name"></span>
+                    <input type="name" className="error" name="name" required onChange={(e) => this.validateData(e, 'name')}/>
                 </div>
                 <div>
                     <label htmlFor="email" className="required" >E-mail</label>
-                    <input type="email" name="email" required onChange={(e) => this.validateData(e, 'email')} />
                     <span className="error-field error-email"></span>
+                    <input type="email" name="email" required onChange={(e) => this.validateData(e, 'email')} />
                 </div>
                 <div>
                     <label htmlFor="phone">Phone</label>
-                    <InputMask type="phone" name="phone" mask="+7 \(999) 999-99-99" placeholder="+7 (___) ___-__-__"/>
+                    <InputMask type="phone" name="phone" mask="+7 \(999) 999-99-99" placeholder="+7 (___) ___-__-__" onChange={(e) => this.validateData(e, 'phone')} />
                 </div>
                 
                 <label htmlFor="message" className="required">Your message</label>
                 <div>
-                    <input type="text" className="textarea" name="message" required placeholder="Text us..." onChange={(e) => this.validateData(e, 'message')} />
                     <span className="error-field error-message"></span>
+                    <input type="text" className="textarea" name="message" required placeholder="Text us..." onChange={(e) => this.validateData(e, 'message')} />
                 </div>
                 
                 <button type="submit" >
